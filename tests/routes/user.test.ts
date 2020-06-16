893d4657de528f1214d7143fd1ee108a2fe3f0d6
omerdemirkan <omerfarukpiano@gmail.com>
2020-06-15 18:47:42 -0700

    embed user.test.ts in app promise

--- tests/routes/user.test.ts
+++ tests/routes/user.test.ts
@@ -1,46 +1,50 @@
-import app from "../../src/app";
+import serverRunning from "../../src/app";
 const request = require("supertest");
 
-describe("/api/user", () => {
-  // beforeEach((done) => {
-  //   app.on("serverInitialized", done);
-  // });
+serverRunning.then(function(app) {
 
-  const userTests = (userRole: string) => {
-    describe(`GET - ${userRole}`, () => {
-      it(`should get all ${userRole}s`, async () => {
-        const res = await request(app).get(`api/user/${userRole}`);
-        expect(res).toBeDefined();
+  describe("/api/user", () => {
+    // beforeEach((done) => {
+    //   app.on("serverInitialized", done);
+    // });
+  
+    const userTests = (userRole: string) => {
+      describe(`GET - ${userRole}`, () => {
+        it(`should get all ${userRole}s`, async () => {
+          const res = await request(app).get(`api/user/${userRole}`);
+          expect(res).toBeDefined();
+        });
+  
+        it(`should get ${userRole} by id`, () => {
+          // TODO
+        });
       });
-
-      it(`should get ${userRole} by id`, () => {
-        // TODO
+  
+      const methodTests = (method: "PATCH" | "DELETE") => {
+        it(`should ${method} ${userRole} if authorized and id matches access-token`, () => {
+          // TODO
+        });
+  
+        it("should fail if not authorized", () => {
+          // TODO
+        });
+  
+        it("should fail if id doesn't match access-token", () => {
+          // TODO
+        });
+      };
+  
+      describe(`PATCH - ${userRole}`, () => {
+        methodTests("PATCH");
       });
-    });
-
-    const methodTests = (method: "PATCH" | "DELETE") => {
-      it(`should ${method} ${userRole} if authorized and id matches access-token`, () => {
-        // TODO
-      });
-
-      it("should fail if not authorized", () => {
-        // TODO
-      });
-
-      it("should fail if id doesn't match access-token", () => {
-        // TODO
+  
+      describe(`DELETE - ${userRole}`, () => {
+        methodTests("DELETE");
       });
     };
-
-    describe(`PATCH - ${userRole}`, () => {
-      methodTests("PATCH");
-    });
-
-    describe(`DELETE - ${userRole}`, () => {
-      methodTests("DELETE");
-    });
-  };
-
-  const userRoleEnpoints = ["student", "instructor", "school-official"];
-  userRoleEnpoints.map(userTests);
-});
+  
+    const userRoleEnpoints = ["student", "instructor", "school-official"];
+    userRoleEnpoints.map(userTests);
+  });
+  
+})
