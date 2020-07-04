6a036ac711160f32f84c0b6f64aeee2b6597fce7
omerdemirkan <omerfarukpiano@gmail.com>
2020-07-03 21:42:26 -0700

    update config, package.json and docker files for cloud run

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /usr/src/stem-bound
 
 COPY package*.json ./
 
-RUN npm install --only=production
+RUN npm install
 
 COPY . ./
 
@@ -12,4 +12,4 @@ RUN npm run build
 
 EXPOSE 8080
 
-CMD ["npm", "start"]
+CMD ["node", "build/index.js"]
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,18 @@
 import dotenv from "dotenv";
+import { logger } from "./global.config";
 
 const envFound = dotenv.config();
 
 if (envFound.error) {
-    throw new Error("!!! env file not found !!!");
+    logger.error("!!! env file not found !!!");
 }
 
 const config = Object.freeze({
     port: process.env.PORT,
     dbUrl:
-        process.env.NODE_ENV === "development"
-            ? process.env.LOCAL_DB_URL
-            : process.env.CLOUD_DB_URL,
+        process.env.NODE_ENV === "production"
+            ? process.env.CLOUD_DB_URL
+            : process.env.LOCAL_DB_URL,
     accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
     saltRounds: Number(process.env.SALT_ROUNDS),
     mailgunApiKey: process.env.MAILGUN_API_KEY,
@@ -19,7 +20,7 @@ const config = Object.freeze({
 });
 
 if (Object.values(config).includes(undefined)) {
-    throw new Error(`Some .env variables are missing. config: ${config}`);
+    logger.error(`Some .env variables are missing. config: ${config}`);
 }
 
 export default config;
