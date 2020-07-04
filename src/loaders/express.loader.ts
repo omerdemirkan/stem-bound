52d76a84776d159361b556825a3ac298bc221556
omerdemirkan <omerfarukpiano@gmail.com>
2020-07-03 20:31:02 -0700

    add loopback configuration (to "0.0.0.0")

--- src/loaders/express.loader.ts
+++ src/loaders/express.loader.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 
 export default function (app: Application) {
     app.set("trust proxy", 1);
+    app.set("trust proxy", "loopback, 0.0.0.0");
     app.use(express.json());
     app.use(cors());
 }
