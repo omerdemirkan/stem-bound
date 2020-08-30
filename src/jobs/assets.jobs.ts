c10887235a10da65b3e9c6d3a2865f3a3337bfa0
omerdemirkan <omerfarukpiano@gmail.com>
2020-08-30 09:43:39 -0700

    create saveFileToBucket job

--- src/jobs/assets.jobs.ts
+++ src/jobs/assets.jobs.ts
@@ -2,6 +2,23 @@ import config, { storage } from "../config";
 
 const bucket = storage.bucket(config.assetsBucketName);
 
-export function saveRawImageToBucket() {
-    storage.bucket;
+export function saveFileToBucket(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const { name, arrayBuffer } = file;
+
+        const blob = bucket.file(name.replace(/ /g, "_"));
+        const blobStream = blob.createWriteStream({
+            resumable: false,
+        });
+        blobStream
+            .on("finish", () => {
+                resolve(
+                    `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+                );
+            })
+            .on("error", () => {
+                reject(`Unable to upload image, something went wrong`);
+            })
+            .end(arrayBuffer);
+    });
 }
