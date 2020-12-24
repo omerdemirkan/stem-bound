38cad4802d10a0e4d48dbb5754b696bfa83bd873
omerdemirkan <omerfarukpiano@gmail.com>
2020-12-24 14:19:42 -0800

    fix course array unauthenticated fetch error

--- src/helpers/course.helpers.ts
+++ src/helpers/course.helpers.ts
@@ -1,37 +1,67 @@
 import { Types } from "mongoose";
 import {
     ICourse,
-    ITokenPayload,
     IMeeting,
     IQuery,
     ISubDocumentQuery,
     IRequestMetadata,
     IAnnouncement,
 } from "../types";
+const { ObjectId } = Types;
 
 export function configureCourseArrayQuery(
     requestMetadata: IRequestMetadata
 ): IQuery<ICourse> {
-    let { skip, limit, school_id, unverified } = requestMetadata.query;
+    let {
+        skip,
+        limit,
+        unverified,
+        school_id,
+        instructor_id,
+        student_id,
+    } = requestMetadata.query;
+
     skip = +skip;
     limit = +limit;
     unverified = !!unverified;
-    school_id = school_id ? Types.ObjectId(school_id) : null;
+    school_id = ObjectId.isValid(school_id) ? ObjectId(school_id) : null;
+    instructor_id = ObjectId.isValid(instructor_id)
+        ? ObjectId(instructor_id)
+        : null;
+    student_id = ObjectId.isValid(student_id) ? ObjectId(student_id) : null;
+
     let query: IQuery<ICourse> = { filter: {} };
 
     if (skip) query.skip = skip;
     if (limit) query.limit = limit;
     if (unverified) query.filter.verified = false;
+
     // @ts-ignore
-    if (school_id) query.filter.meta = { school: school_id };
+    if (school_id) query.filter["meta.school"] = school_id;
+    // @ts-ignore
+    if (instructor_id) query.filter["meta.instructors"] = instructor_id;
+    // @ts-ignore
+    if (student_id) query.filter["meta.students"] = student_id;
+    console.log(query);
     return query;
 }
 
 export function configureMeetingArrayQuery(
     requestMetadata: IRequestMetadata
 ): ISubDocumentQuery<IMeeting> {
-    let {} = requestMetadata.query;
-    let query = {};
+    let { before, after, limit, skip } = requestMetadata.query;
+
+    before = before ? new Date(before) : null;
+    after = after ? new Date(after) : null;
+    limit = +limit;
+    skip = +skip;
+
+    let query: ISubDocumentQuery<IMeeting> = {};
+
+    if (before) query.before = before;
+    if (after) query.after = after;
+    if (limit) query.limit = limit;
+    if (skip) query.skip = skip;
     return query;
 }
 
@@ -54,16 +84,18 @@ export function configureCourseArrayResponseData(
         meetings: course.meetings
             .filter((meeting) => meeting.end > now)
             .slice(0, 11),
-        announcements: course.meta.instructors
-            .map((id) => id.toString())
-            .includes(payload.user._id)
-            ? []
-            : course.announcements.filter(
-                  (announcement) =>
-                      !announcement.meta.readBy
-                          .map((id) => id.toString())
-                          .includes(payload.user._id)
-              ),
+        announcements:
+            !payload?.user ||
+            course.meta.instructors
+                .map((id) => id.toString())
+                .includes(payload?.user._id)
+                ? []
+                : course.announcements.filter(
+                      (announcement) =>
+                          !announcement.meta.readBy
+                              .map((id) => id.toString())
+                              .includes(payload.user._id)
+                  ),
     }));
     return configuredCourses;
 }
