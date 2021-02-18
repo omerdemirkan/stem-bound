dbd6ae6fc5d105ceaf2f845c59b2b7d909cacae6
Omer Demirkan <omerfarukpiano@gmail.com>
2021-02-18 12:05:43 -0800

    add meeting time validation helper function

--- src/helpers/course.helpers.ts
+++ src/helpers/course.helpers.ts
@@ -187,3 +187,39 @@ export function isValidCourseType(courseType: ECourseTypes): boolean {
 export function isValidMeetingType(meetingType: EMeetingTypes): boolean {
     return Object.keys(EMeetingTypes).includes(meetingType);
 }
+
+export function validateMeetingTimes(meetings: IMeeting[]) {
+    // -1 means an ending state change, 1 means starting.
+    let stateChanges: { step: 1 | -1; time: Date; meetingId: string }[] = [];
+
+    for (let meeting of meetings) {
+        stateChanges.push({
+            step: 1,
+            time: new Date(meeting.start),
+            meetingId: meeting._id.toString(),
+        });
+        stateChanges.push({
+            step: -1,
+            time: new Date(meeting.end),
+            meetingId: meeting._id.toString(),
+        });
+    }
+
+    stateChanges = stateChanges.sort(function (a, b) {
+        if (a.time !== b.time) return a.time.getTime() - b.time.getTime();
+        // If a is an ending state change (i.e step is -1)
+        // a takes precedence, otherwise b takes precedence.
+        // Note that its impossible for a.step === b.step.
+        return a.step;
+    });
+
+    let concurrentInstructions = 0;
+
+    for (let stateChange of stateChanges) {
+        concurrentInstructions += stateChange.step;
+        if (concurrentInstructions > 1 || concurrentInstructions < 0)
+            return false;
+    }
+
+    return true;
+}
